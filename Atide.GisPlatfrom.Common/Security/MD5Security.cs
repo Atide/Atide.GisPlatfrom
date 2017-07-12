4c997fd5f4156910e4e4508b0870d0504cbde54e
liujiajiaskk <liujiajiaskk@163.com>
2017-07-12 14:37:19 +0800

    vs 提交测试

--- Atide.GisPlatfrom.Common/Security/MD5Security.cs
+++ Atide.GisPlatfrom.Common/Security/MD5Security.cs
@@ -8,7 +8,7 @@ namespace Atide.GisPlatfrom.Common.Security
     public class MD5Security
     {
         /// <summary>
-        /// netcore下的实现MD5加密
+        /// netcore下的实现MD5加密vstest
         /// </summary>
         /// <param name="str"></param>
         /// <returns></returns>
